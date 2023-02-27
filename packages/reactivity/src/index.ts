f249b0f94bfbfcfc093c91f02b6f945624627ad4
yang <1372201820@qq.com>
2023-02-27 22:32:38 +0800

    feat:基础开发环境搭建

---
+++ packages/reactivity/src/index.ts
@@ -0,0 +1,4 @@
+let a = 1;
+let f = 2;
+
+export { a, f };
--- scripts/build.js
+++ scripts/build.js
@@ -10,4 +10,22 @@ const dirs = fs.readdirSync('packages').filter(p => {
   return true;
 });
 
-console.log(dirs);
+async function build(target) {
+  // 注意 execa  -c 执行rullup配置 ， 环境变量 -env
+  await execa('rollup', ['-c', '--environment', `TARGET:${target}`], { stdio: 'inherit' }); // 子进程的输出在父包中输出
+}
+
+async function runParaller(dirs, itemfn) {
+  //遍历
+  let result = [];
+  for (let item of dirs) {
+    result.push(itemfn(item));
+  }
+  return Promise.all(result); // 存放打包的promise ,等待这里的打包执行完毕之后，调用成功
+}
+
+// 并发打包
+runParaller(dirs, build).then(() => {
+  //promise
+  console.log('成功');
+});
