739310dc7313fa19770c2b67b57fd9279fa85fe6
yang <1372201820@qq.com>
2023-03-02 23:05:03 +0800

    feat:处理收集依赖

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,4 +1,4 @@
-import { isObject } from '@vue/shared';
+import { isObject, isArray, isIntegerKey, hasOwn } from '@vue/shared';
 import { readonly, reative } from './reactiveApi';
 import { TrackOptypes } from './optionations';
 import { Track } from './effect';
@@ -26,11 +26,21 @@ function createGetter(isReadOnly = false, shallow = false) {
 function createSetter(shallow = false) {
   //拦截设置的功能
   return function set(target, key, value, receiver) {
-    //state.name = 100
-    const result = Reflect.set(target, key, value, receiver); // target[key] = value
-    //当数据更新时候 通知对应属性的effect重新执行
-    // 我们要区分是新增的 还是 修改的  vue2 里无法监控更改索引，无法监控数组的长度变化
-    // =》 hack的方法  需要特殊处理
+    const result = Reflect.set(target, key, value, receiver);
+    // 获取老值
+    const oldValue = target[key];
+    // 判断target(数组或对象)中是否有key这个属性
+    let haskey =
+      isArray(target) && isIntegerKey(key) ? Number(key) < target.length : hasOwn(target, key);
+
+    if (!haskey) {
+      //没有
+      //新增
+    } else {
+      //修改的时候 新值和原来的值一样
+      //    trigger(target,)
+    }
+
     return result;
   };
 }
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -16,7 +16,7 @@ let activeEffect;
 //创建一个栈
 const effectStack = [];
 function createReactiveEffect(fn, options) {
-  /*  通过栈解决一下问题
+  /*  通过栈解决以下问题
  effect(() => { [effect1]<-cur
     state.name;
     effect(() => { [effect1,effect2]<-cur
@@ -26,15 +26,17 @@ function createReactiveEffect(fn, options) {
   }); 
   */
   const effect = function reactiveEffect() {
-    try {
-      effectStack.push(effect);
-      activeEffect = effect;
-      // 执行用户定义的方法
-      fn();
-    } finally {
-      //出栈
-      effectStack.pop();
-      activeEffect = effectStack[effectStack.length - 1];
+    if (!effectStack.includes(effect)) {
+      try {
+        effectStack.push(effect);
+        activeEffect = effect;
+        // 执行用户定义的方法
+        fn();
+      } finally {
+        //出栈
+        effectStack.pop();
+        activeEffect = effectStack[effectStack.length - 1];
+      }
     }
   };
   effect.id = uid++; //添加标识，用于区分effect(是谁的)
@@ -44,6 +46,38 @@ function createReactiveEffect(fn, options) {
   return effect;
 }
 
+/*
+ *targetMap结构：{
+ *  key:被代理的对象(Object)
+ *  value:Map{
+ *    key:属性名
+ *    value:属性名对应的依赖(Set<Function>)
+ *  }
+ *}
+ */
+let targetMap = new WeakMap();
 export function Track(target, type, key) {
   console.log(target, type, key, activeEffect);
+  if (!activeEffect) {
+    // 没有在effect中使用 直接中断
+    return;
+  }
+  // 获取effect
+  let depsMap = targetMap.get(target);
+  //没有
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map())); //第二个参数是不是他
+  }
+  //有
+  let dep = depsMap.get(key);
+  if (!dep) {
+    //没有key
+    depsMap.set(key, (dep = new Set()));
+  }
+  //设置  set
+  if (!dep.has(activeEffect)) {
+    //没有
+    dep.add(activeEffect);
+  }
+  console.log(targetMap);
 }
