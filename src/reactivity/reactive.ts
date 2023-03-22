75700688d1d46109cff349780b9925c76fe9932e
yang <1372201820@qq.com>
2023-03-22 22:28:35 +0800

    feat:实现readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,18 +1,13 @@
-import { track, trigger } from './effect';
+import { mutableHandlers, readonlyHandlers } from './baseHandlers';
 
-export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      // TODO 依赖收集
-      track(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      // TODO  触发依赖
-      trigger(target, key);
-      return res;
-    },
-  });
+function createActiveObject(raw, baseHandlers) {
+  return new Proxy(raw, baseHandlers);
+}
+
+export function reactive(raw: any) {
+  return createActiveObject(raw, mutableHandlers);
+}
+
+export function readonly(raw: any) {
+  return createActiveObject(raw, readonlyHandlers);
 }
