744a107dc11705d3abc616e83bea52888f1fd310
yang <1372201820@qq.com>
2023-03-27 21:24:48 +0800

    feat：实现isRef和unRef

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any; // 存的是运算时的数据，如果是对象会被proxy代理
   private _rawValue: any; // 存储最原始的传入数据,用于判断是否改变
   public dep;
+  public __v_isRef = true;
   constructor(value) {
     this._rawValue = value;
     // 看看传进来的数据是不是对象
@@ -36,4 +37,12 @@ function convert(value) {
 
 export function ref(value) {
   return new RefImpl(value);
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref;
 }
